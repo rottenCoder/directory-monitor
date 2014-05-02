806277f123769c0e948c547c0f411070b8254689
Rowan Lewis <rl@nbsp.io>
2014-05-02 13:52:34 +0300

    Added installer for Inotify extension.

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -2,5 +2,13 @@
 set -e
 set -o pipefail
 
+if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then
+
+    # install 'inotify' PHP extension
+    echo "yes" | pecl install inotify
+    echo "extension=inotify.so" >> "$(php -r 'echo php_ini_loaded_file();')"
+
+fi
+
 composer self-update
 composer install --dev --prefer-source
